cb7ea560547e0b0dba96bc67a6d919d8b8dc6420
dimakovalev25 <d.nozdryn@gmail.com>
2022-09-24 16:22:16 +0300

    timer

--- js/main.js
+++ js/main.js
@@ -2,39 +2,39 @@
 
 window.addEventListener("DOMContentLoaded", () => {
 
-	const tabs = document.querySelectorAll(".tabheader__item");
-	const tabsContent = document.querySelectorAll(".tabcontent");
-	const tabsParent = document.querySelector(".tabheader__items");
-
-	console.log(tabsContent);
+	const tabs = document.querySelectorAll('.tabheader__item');
+	const tabsContent = document.querySelectorAll('.tabcontent');
+	const tabsParent = document.querySelector('.tabheader__items');
+	
+	// console.log(tabsContent);
+	// console.log(tabsParent);
 
 	function hideTabContent() {
 		tabsContent.forEach(item => {
-			item.style.display = "none";
+			item.style.display = 'none';
 		});
-
-		tabs.forEach(item => {
-			item.classList.remove('tabheader__item_active');
+		tabs.forEach(tab => {
+			tab.classList.remove('tabheader__item_active');
 		});
-	} 
-
-	function showTabContect(i = 0) {
-		tabsContent[i].style.display = 'block';
-		tabs[i].classList.add('tabheader__item_active');
-
 	}
 
-	hideTabContent();
-	showTabContect();
+	function showTabContent(i = 0) {
+		tabsContent[i].style.display = 'block';
+		tabs[i].classList.add('tabheader__item_active');
+	}
+
+	
+		hideTabContent();
+		showTabContent();
 
 	tabsParent.addEventListener('click', (event) => {
 		const target = event.target;
 
-		if(target && target.classList.contains('tabheader__item')) {
+		if(target && target.classList.contains("tabheader__item")) {
 			tabs.forEach((item, i) => {
 				if (target == item) {
 					hideTabContent();
-					showTabContect(i);
+					showTabContent(i);
 				}
 			});
 		}
@@ -42,6 +42,78 @@ window.addEventListener("DOMContentLoaded", () => {
 	});
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.addEventListener("DOMContentLoaded", () => {
+
+// 	const tabs = document.querySelectorAll(".tabheader__item");
+// 	const tabsContent = document.querySelectorAll(".tabcontent");
+// 	const tabsParent = document.querySelector(".tabheader__items");
+
+// 	console.log(tabsContent);
+
+// 	function hideTabContent() {
+// 		tabsContent.forEach(item => {
+// 			item.style.display = "none";
+// 		});
+
+// 		tabs.forEach(item => {
+// 			item.classList.remove('tabheader__item_active');
+// 		});
+// 	} 
+
+// 	function showTabContect(i = 0) {
+// 		tabsContent[i].style.display = 'block';
+// 		tabs[i].classList.add('tabheader__item_active');
+
+// 	}
+
+// 	hideTabContent();
+// 	showTabContect();
+
+// 	tabsParent.addEventListener('click', (event) => {
+// 		const target = event.target;
+
+// 		if(target && target.classList.contains('tabheader__item')) {
+// 			tabs.forEach((item, i) => {
+// 				if (target == item) {
+// 					hideTabContent();
+// 					showTabContect(i);
+// 				}
+// 			});
+// 		}
+
+// 	});
+
+// });
 
 
 
--- js/set.js
+++ js/set.js
@@ -32,3 +32,65 @@ console.log(set.size);
 
 
 
+// amountOfPages(5) => 5
+// amountOfPages(25) => 17
+// amountOfPages(1095) => 401   
+
+function amountOfPages(n) {
+	let res = [];
+	for (let i = 1; i <= n; i++) {
+		if (res.length < n) {
+			res.push(i);
+			res = res.join("").split("");
+		} else if (res.length == n) {
+			break;
+		}
+	}
+	res = res.join("");
+	// console.log(res.length);
+
+	if (res.length <= 9) {
+		console.log(res.slice(-1,));
+		return res.slice(-1, );
+	} else if (res.length > 9 && res.length <= 189) {
+		console.log(res.slice(-2, ));
+		return res.slice(-2, );
+	} else if (res.length > 189) {
+		console.log(res.slice(-3, ));
+		return res.slice(-3, );
+	}
+}
+amountOfPages(11);
+
+
+function amountOfPages(summary) {
+	let n = summary;
+	let res = [];
+	for (let i = 1; i <= n; i++) {
+		if (res.length < n) {
+			res.push(i);
+			res = res.join("").split("");
+		} else if (res.length == n) {
+			break;
+		}
+	}
+	res = res.join("");
+	// console.log(res.length);
+
+	if (res.length <= 9) {
+		console.log(res.slice(-1,));
+		return res.slice(-1, );
+	} else if (res.length > 9 && res.length <= 189) {
+		console.log(res.slice(-2, ));
+		return res.slice(-2, );
+	} else if (res.length > 189) {
+		console.log(res.slice(-3, ));
+		return res.slice(-3, );
+	}
+}
+amountOfPages(185);
+amountOfPages(1095);
+amountOfPages(189);
+amountOfPages(25);
+amountOfPages(5);
+amountOfPages(9);
